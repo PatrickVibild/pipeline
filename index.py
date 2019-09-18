8ab3869187d44aef73a38014227c47bf042c4c59
patrick <patrickvibild@gmail.com>
2019-09-18 14:57:47 +0200

    lets trigger the pipeline

--- index.py
+++ index.py
@@ -8,8 +8,9 @@ def index():
   <p>A sample web-app for running Flask inside Docker.</p>
   <p> We have make some changes!!! YUPI YUPI YEIIIII !! </p>
   <p> Lets code this shit out </p>
-  <p> hello DEMIRA its me PATRICK!!!! </p>
   <p> lets trigger the pipeline!!! </p>
+  
+<p> trigger pipe</p>
   """
 
 if __name__ == "__main__":
